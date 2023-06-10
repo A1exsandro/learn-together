c36b21884877779885ca7c3331ce38dfccd7db37
A1exsandro <alexsandro.acp@gmail.com>
2023-06-10 15:56:11 -0400

    feat: add home and layout components

---
+++ src/pages/Home.jsx
@@ -0,0 +1,12 @@
+import React from "react" 
+
+const Home = () => { 
+
+  return(
+    <div className="flex justify-center items-center my-4">
+      <div>Learn Together</div> 
+    </div>
+  )
+}
+
+export default Home
---
+++ src/pages/Layout.jsx
@@ -0,0 +1,46 @@
+import React, { useState } from "react"
+import { Outlet, Link } from "react-router-dom"  
+
+const Layout = () => {  
+  const [none, setNone] = useState(false)
+
+  const toggleMenu = () => {
+    setNone(!none)
+  }   
+
+  return (
+    <>
+      <nav className="flex justify-between items-center bg-black">
+        <div 
+          className="bg-black text-white px-5 py-2 hover:bg-slate-500
+          hover:cursor-pointer" 
+          onClick={toggleMenu}
+        >
+          Menu
+        </div>  
+      </nav>
+      
+      <div className={` ${none ? "" : "hidden"} bg-black text-white`}>
+        <div className="flex flex-col w-1/3 p-2">
+
+					<div 
+            onClick={toggleMenu}
+            className="hover:bg-slate-500 hover:cursor-pointer
+            hover:pl-4 hover:text-lime-500"
+          ><Link to="/">Home</Link></div>
+       
+          <div 
+            onClick={toggleMenu}
+            className="hover:bg-slate-500 hover:cursor-pointer
+            hover:pl-4 hover:text-lime-500"
+          ><Link to="/tictactoe">Jogo da Velha</Link></div>
+
+        </div> 
+      </div>
+
+      <Outlet />
+    </>
+  )
+};
+  
+export default Layout
