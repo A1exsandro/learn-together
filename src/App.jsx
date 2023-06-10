9a20d4d690983687c51886dafc688e09e99d0904
A1exsandro <alexsandro.acp@gmail.com>
2023-06-10 15:17:19 -0400

    feat: clean the project and install tailwind

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import React from "react"
+
+const App = () => {
+
+  return (
+    <div 
+      className="bg-black text-white"
+    >
+      Learn Together
+    </div>
+  )
+}
+
+export default App
