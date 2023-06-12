43e616cf6caa3807ea988f81858bafb9127ef11a
A1exsandro <alexsandro.acp@gmail.com>
2023-06-12 09:31:55 -0400

    feat: score component

--- src/components/memory-game/Score.jsx
+++ src/components/memory-game/Score.jsx
@@ -1,7 +1,19 @@
 
 const Score = () => {
   return (
-    <div>Score</div>
+    <div className="flex justify-between mb-1">
+      <Points titulo="Score" value={0} />
+      <Points titulo="Card Flipped" value={0} />
+    </div>
+  )
+}
+
+const Points = ({titulo, value}) => {
+  return (
+    <div className="">
+      <strong>{titulo}:</strong>
+      <span className="ml-2">{value}</span>
+    </div>
   )
 }
 
