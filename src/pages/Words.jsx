741918513a7c72441b1acbddcbfa07a6981fc617
A1exsandro <alexsandro.acp@gmail.com>
2023-06-14 12:38:34 -0400

    refactor: clean the word component

--- src/pages/Words.jsx
+++ src/pages/Words.jsx
@@ -1,71 +1,43 @@
 import React, { useEffect, useState } from "react" 
 
-import { getStorage } from '../services/Firebase'
-import { ref, getDownloadURL } from "firebase/storage"  
 import Loader from "../components/Loader"
-import Card from "../components/Card"
+import Card from "../components/Card" 
+import { useMemory } from "../contexts/MemoryContext"
 
-const storage = getStorage()
-
-const data = [
-  'butterfly',
-  'avocado',
-  'banana',
-  'bathroom',
-  'bedroom',
-  'breakfast',
-  'bridge',
-  'brothers', 
-  'closed',
-  'cook',
-  'dance',
-  'eat',
-  'fear',
-  'fire',
-  'film',
-  'fruits',
-  'kitchen',
-  'night',
-  'read',
-  'open',
-  'marketplace',
-  'play',
-  'smell',
-  'sleep',
-  'plant',
-  'tired'
-] 
+// const data = [
+//   'butterfly',
+//   'avocado',
+//   'banana',
+//   'bathroom',
+//   'bedroom',
+//   'breakfast',
+//   'bridge',
+//   'brothers', 
+//   'closed',
+//   'cook',
+//   'dance',
+//   'eat',
+//   'fear',
+//   'fire',
+//   'film',
+//   'fruits',
+//   'kitchen',
+//   'night',
+//   'read',
+//   'open',
+//   'marketplace',
+//   'play',
+//   'smell',
+//   'sleep',
+//   'plant',
+//   'tired'
+// ] 
 
 const Words = () => { 
-  const [images, setImages] = useState([])
-  const [sounds, setSounds] = useState([])
-  const [cards] = useState([{}])
+  const { cards } = useMemory() 
  
-  useEffect(() => {
-    const promises = data.map((dt) => (
-      getDownloadURL(ref(storage, `images/${dt}.jpeg`))
-    ))
-  
-    const audioPromises = data.map((dt) => (
-      getDownloadURL(ref(storage, `audio/${dt}.mp3`))
-    ))
-
-    Promise.all(promises)
-      .then((urls) => setImages(urls)) 
-
-    Promise.all(audioPromises)
-      .then((audios) => setSounds(audios)) 
-  },[])
-
-  // CREATING AN OBJECT THROUGH ARRAY INTERACTION
-  for (let i = 0; i < data.length; i++) {
-    cards[i] = {
-      id: i +1,
-      nameImg: data[i],
-      urlImg: images[i],
-      urlSound: sounds[i],
-    }
-  } 
+  console.log(cards)
+  //console.log('data', data)
  
   return (
     <div className="flex flex-col">
@@ -74,7 +46,7 @@ const Words = () => {
         className="grid grid-cols-4 gap-4 p-4"
       >
       {
-        ( sounds.length > 0  && 
+        ( cards.length > 0  &&
           cards.map((card, i) => (
             <Card key={i} {...card} />
           ))
