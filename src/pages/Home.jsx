be630038c0575ffaff0aa191cabbc26747ae2acd
A1exsandro <alexsandro.acp@gmail.com>
2023-06-16 14:46:07 -0400

    feat: add content in home page

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,10 +1,22 @@
 import React from "react" 
 
 const Home = () => { 
+  const pages = ['Day Of Week', 'Month Names', 'Greetings', 'Colors', 'Body Parts', 'Phrasal verbs']
 
   return(
-    <div className="flex justify-center items-center my-4">
-      <div>Learn Together</div> 
+    <div className="flex justify-center items-center p-4">
+      <div className="w-full grid grid-cols-1 md:grid-cols-3 gap-4">
+        {
+          pages.map((page, i) => (
+            <div 
+              key={i} 
+              className="bg-white aspect-[12/4] rounded-xl flex justify-center items-center"
+            >
+              {page}
+            </div>
+          ))
+        }
+      </div> 
     </div>
   )
 }
