8fcaac8b4a0a2580638d554b385a3873c7e4781c
A1exsandro <alexsandro.acp@gmail.com>
2023-06-19 16:42:10 -0400

    feat: mock data

--- src/pages/WeekDays.jsx
+++ src/pages/WeekDays.jsx
@@ -1,10 +1,22 @@
 import React from "react"
 
 const WeekDays = () => {
+  const data = ['Sunday', 'Monday', 'Tuesday', 'Wednesday', 
+  'Thursday', 'Friday', 'Saturday']
 
   return (
-    <div>
+    <div className="flex flex-col items-center gap-4 mt-4 w-80 m-auto">
       Day of the week
+      {
+        data.map((day,i) => (
+          <div 
+            className="bg-white w-full rounded-full text-center py-4"
+            key={i}
+          >
+            {day}
+          </div>
+        ))
+      }
     </div>
   )
 }
