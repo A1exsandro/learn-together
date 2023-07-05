42388891b30b7c3e2aaae6efdc2572c24dc88e38
A1exsandro <alexsandro.acp@gmail.com>
2023-07-05 18:51:37 -0400

    refactor: navbar

--- src/layout/Layout.jsx
+++ src/layout/Layout.jsx
@@ -10,17 +10,18 @@ const Layout = () => {
 
   return (
     <>
-      <nav className="flex justify-between items-center bg-black">
+      <nav className="w-full flex items-center py-2 fixed top-0
+      bg-black mb-2">
         <div 
           className="bg-black text-white px-5 py-2 hover:bg-slate-500
-          hover:cursor-pointer" 
+          hover:cursor-pointer rounded-full" 
           onClick={toggleMenu}
         >
           Menu
         </div>  
       </nav>
       
-      <div className={` ${none ? "" : "hidden"} bg-black text-white`}>
+      <div className={` ${none ? "" : "hidden"} fixed w-full mt-12 bg-black text-white`}>
         <div className="flex flex-col w-1/3 p-2">
 
 					<div 
@@ -43,6 +44,8 @@ const Layout = () => {
 
         </div> 
       </div>
+
+      <div className="w-full h-20 "></div>
       
       <Outlet />
     </>
