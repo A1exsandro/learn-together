17938222e8ebc5f5aa34707bb6f59f33c4555676
A1exsandro <alexsandro.acp@gmail.com>
2023-07-17 15:20:48 -0400

    feat: add cards in listen

--- src/pages/Listen.jsx
+++ src/pages/Listen.jsx
@@ -1,10 +1,43 @@
-import React from "react"
+import React, { useEffect, useRef } from "react" 
+import { useFetch } from "../hooks/useFetch";
 
 const Listen = () => {
+  const { makedCards } = useFetch()
 
   return (
-    <div>
-      Listen
+    <div 
+      className=" flex flex-col items-center"
+    >
+      <div className="bg-black text-white px-4 mb-3 rounded-xl">
+        { makedCards.length} 
+      </div>
+       
+      <div className="flex justify-center flex-wrap gap-3">
+        { 
+          makedCards.map((card, i) => ( 
+            
+            <div 
+              key={i}  
+              className="bg-white flex flex-col items-center w-20 h-30
+              rounded-xl"
+            >  
+              <img 
+                className="w-20 h-20 rounded-t-xl"
+                src={card.urlImg} 
+                alt={card.urlImg} 
+              />
+
+              <audio>
+                <source src={card.urlSound} /> 
+              </audio>
+
+              <div className="">
+                { card.nameImg}
+              </div>
+            </div>
+          ))
+        }
+      </div>
     </div>
   )
 }
